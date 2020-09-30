4002783a4f5aefa8e1d2d1566db4bd0a8018bc31
Andre Postiga <andre.postiga@msn.com>
2020-09-29 21:13:24 -0300

    feat: add description to model

--- src/Models/Product.cs
+++ src/Models/Product.cs
@@ -7,18 +7,28 @@ namespace FakeOmmerce.Models
 
   public class Product
   {
+
+    // private ObjectId id;
+    // private string name;    
+    // private List<string> images;    
+    // private List<string> categories;
+    // private double price;
+    // private string brand;
+    // private string description;
+
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
+    [BsonElement("id")]
     public string Id { get; set; }
 
     [BsonElement("name")]
     [Required]
     [MaxLength(60)]
     [MinLength(3)]   
-    public string Name { get; set;}
+    public string Name { get; set; }
 
     [BsonElement("images")]
-    public List<string> Images;
+    public List<string> Images { get; set; }
     
     [BsonElement("categories")]
     public HashSet<string> Categories { get; set; }
@@ -30,8 +40,27 @@ namespace FakeOmmerce.Models
 
     [Required]
     [BsonElement("brand")]
+    [Required]
     public string Brand { get; set; }
 
+    [BsonElement("description")]
+    public string Description { get; set; }
 
+    [BsonElement("categories")]
+    [Required]
+    public List<string> Categories { get; set; }   
+
+    public Product()
+    { }
+
+    public Product(string name, List<string> images, List<string> categories, double price, string brand, string description)
+    {
+      Name = name;
+      Images = images;
+      Categories = categories;
+      Price = price;
+      Brand = brand;
+      Description = description;
+    }
   }
 }
